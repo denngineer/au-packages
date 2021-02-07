29be61c35e32eae11ff92e766902d32ce13d9d2e
denngineer <78664103+denngineer@users.noreply.github.com>
2021-02-07 14:59:57 +0100

    Add ericards package

---
+++ ericards/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'http://ericards.net/download/ERICardsOffline_3_9_1.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  softwareName  = 'Cefic ERICards Offline'
+  checksum      = '4e3ed810c38eaa65d387efa6952d4e23edc7e2cb47827065b7e61a2390b5b87f'
+  checksumType  = 'sha256'
+  silentArgs   = '/S'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ ericards/update.ps1
@@ -0,0 +1,30 @@
+﻿import-module au
+
+$domain   = 'http://ericards.net'
+$releases = "$domain/psp/ericards.psp_downloads"
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\`$url\s*=\s*)('.*')"        = "`$1'$($Latest.URL32)'"
+      "(?i)(^\s*checksum\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum32)'"
+      "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $download_page = Invoke-WebRequest -UseBasicParsing -Uri $releases
+
+  $re    = '\.exe$'
+  $url   = $download_page.links | ? href -match $re | select -First 1 -expand href
+
+  $version  = Get-Version ( $url -replace '_','.' )
+
+  @{
+    URL32 = $domain + $url
+    Version = $version
+  }
+}
+
+update -ChecksumFor 32
